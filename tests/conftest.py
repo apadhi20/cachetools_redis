4ec8997bed1be50a7db5e93706aae396f06c97db
Daniel Kukula <daniel.kuku@gmail.com>
2021-01-24 11:37:26 +0000

    use pickle for serialization/deserialization

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ from redis import Redis
 
 @pytest.fixture
 def redis():
-    redis_db = Redis(host="localhost", port="6379", db=0, encoding="utf-8", decode_responses=True)
+    redis_db = Redis(host="localhost", port="6379", db=0, encoding="utf-8", decode_responses=False)
     assert redis_db.ping()
     
     return redis_db
